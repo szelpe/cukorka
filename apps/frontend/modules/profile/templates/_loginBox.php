9611f664bb387a55e755d65f00019e8b6434fbf5
szelpe <szelpe@gmail.com>
2010-02-19 19:15:07 +0100

    hibajavitas

--- apps/frontend/modules/profile/templates/_loginBox.php
+++ apps/frontend/modules/profile/templates/_loginBox.php
@@ -1,6 +1,6 @@
 <?php use_helper('I18N') ?>
 <?php if(isset($loginForm)) : ?>
-<form action="<?php echo preg_replace('/http/', 'https', url_for('@sf_guard_signin', true)) ?>" method="post">
+<form action="<?php echo url_for('@sf_guard_signin', true) ?>" method="post">
     
         <?php foreach($loginForm as $field) : ?>
         <?php if(!$field->isHidden()) { echo __($field->renderLabel()); } echo ' ' . $field->render(); ?>
