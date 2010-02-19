6f2714a5d94a998e810a79c054e1f618d30c97f9
szelpe <szelpe@gmail.com>
2010-02-19 17:24:32 +0100

    download megjavítva

--- apps/frontend/modules/download/actions/actions.class.php
+++ apps/frontend/modules/download/actions/actions.class.php
@@ -22,9 +22,21 @@ class downloadActions extends sfActions {
      */
     public function executeIndex(sfWebRequest $request) {
         $path = preg_replace('/\/uploads/', '', preg_replace('/:/', '.', $request->getPathInfo()));
-        $this->response->setHttpHeader('Content-Type', 'application/force-download');
-        $this->response->setHttpHeader('Content-Disposition', 'attachment; filename="' . $path . '"');
-        readfile( sfConfig::get('sf_upload_dir') . $path);
+        $file = sfConfig::get('sf_upload_dir') . $path;
+        if (file_exists($file)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename='.basename($file));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            ob_clean();
+            flush();
+            readfile($file);
+            exit;
+        }
         return sfView::NONE;
     }
 }
