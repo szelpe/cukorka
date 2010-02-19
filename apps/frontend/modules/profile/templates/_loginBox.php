9b97e159aa44333ff4cad76e2bef708c4615f1fd
szelpe <szelpe@gmail.com>
2010-02-19 18:49:47 +0100

    -> https

--- apps/frontend/modules/profile/templates/_loginBox.php
+++ apps/frontend/modules/profile/templates/_loginBox.php
@@ -1,6 +1,6 @@
 <?php use_helper('I18N') ?>
 <?php if(isset($loginForm)) : ?>
-<form action="<?php echo url_for('@sf_guard_signin', true) ?>" method="post">
+<form action="<?php echo preg_replace('/http/', 'https', url_for('@sf_guard_signin', true)) ?>" method="post">
     
         <?php foreach($loginForm as $field) : ?>
         <?php if(!$field->isHidden()) { echo __($field->renderLabel()); } echo ' ' . $field->render(); ?>
@@ -12,4 +12,4 @@
 </form>
 <?php else : ?>
 <?php echo $user->username . ' ' . link_to('Logout', '@sf_guard_signout') ?>
-<?php endif;; ?>
+<?php endif; ?>
